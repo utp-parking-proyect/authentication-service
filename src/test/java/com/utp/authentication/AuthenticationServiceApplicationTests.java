11216be18e7f45ce0d3059e76a93670fe0baff12
sebastian-reyes <sebastianreyes2105@gmail.com>
2025-01-29 23:59:41 -0500

    chore: initial commit

---
+++ src/test/java/com/utp/authentication/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
