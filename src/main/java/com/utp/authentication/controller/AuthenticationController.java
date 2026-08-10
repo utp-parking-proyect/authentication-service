54082fe47af77ce89d5cd8c96bff371559c890d4
sebastian-reyes <sebastianreyes2105@gmail.com>
2026-08-10 03:16:18 -0500

    fix: resolve CORS problem

--- src/main/java/com/utp/authentication/controller/AuthenticationController.java
+++ src/main/java/com/utp/authentication/controller/AuthenticationController.java
@@ -24,7 +24,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
 public class AuthenticationController {
 
   private final AuthenticationManager authenticationManager;
